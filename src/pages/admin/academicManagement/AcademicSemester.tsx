666d97741dc80a484d628df861b7856271a7a965
isalim079 <salimreza1025@gmail.com>
2024-08-22 16:24:10 +0600

    academic semester meta data fixed and types fixed

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,16 +1,88 @@
+import { Table, TableColumnsType, TableProps } from "antd";
 import { academicManagementApi } from "../../../redux/features/admin/academicManagement.api";
 
-
+interface DataType {
+    key: React.Key;
+    name: string;
+    age: number;
+    address: string;
+}
 
 const AcademicSemester = () => {
+    const { data: semesterData } =
+        academicManagementApi.useGetAllSemestersQuery(undefined);
+    console.log(semesterData);
 
-    const {data} = academicManagementApi.useGetAllSemestersQuery(undefined)
+    const tableData = semesterData?.data?.map(
+        ({ _id, name, startMonth, endMonth, year }) => ({
+            _id,
+            name,
+            startMonth,
+            endMonth,
+            year,
+        })
+    );
 
-    console.log(data);
+    const columns: TableColumnsType<DataType> = [
+        {
+            title: "Name",
+            dataIndex: "name",
+            showSorterTooltip: { target: "full-header" },
+            filters: [
+                {
+                    text: "Joe",
+                    value: "Joe",
+                },
+                {
+                    text: "Jim",
+                    value: "Jim",
+                },
+                {
+                    text: "Submenu",
+                    value: "Submenu",
+                    children: [
+                        {
+                            text: "Green",
+                            value: "Green",
+                        },
+                        {
+                            text: "Black",
+                            value: "Black",
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            title: "Year",
+            dataIndex: "year",
+        },
+        {
+            title: "Start Month",
+            dataIndex: "startMonth",
+        },
+        {
+            title: "End Month",
+            dataIndex: "endMonth",
+        },
+    ];
+
+    const onChange: TableProps<DataType>["onChange"] = (
+        pagination,
+        filters,
+        sorter,
+        extra
+    ) => {
+        console.log("params", pagination, filters, sorter, extra);
+    };
 
     return (
         <div>
-            AcademicSemester Component
+            <Table
+                columns={columns}
+                dataSource={tableData}
+                onChange={onChange}
+            />
         </div>
     );
 };
